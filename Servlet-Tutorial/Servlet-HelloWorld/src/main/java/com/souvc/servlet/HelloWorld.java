34d19ea0ec3dd15e2d6dc7e9fd355f2dcc13a463
souvc <291117974@qq.com>
2016-10-16 20:20:40 +0800

    添加Servlet-Tutorial

---
+++ Servlet-Tutorial/Servlet-HelloWorld/src/main/java/com/souvc/servlet/HelloWorld.java
@@ -0,0 +1,41 @@
+package com.souvc.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//导入必需的 java 库
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// 扩展 HttpServlet 类
+public class HelloWorld extends HttpServlet {
+	
+	private static final long serialVersionUID = -8767040769786254282L;
+	
+	private String message;
+
+	public void init() throws ServletException {
+		// 执行必需的初始化
+		message = "Hello World";
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		// 设置响应内容类型
+		response.setContentType("text/html");
+		// 实际的逻辑是在这里
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + message + ",welcome to Servlet world</h1>");
+	}
+
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+		
+	}
+	
+	public void destroy() {
+		// 什么也不做
+	}
+}
