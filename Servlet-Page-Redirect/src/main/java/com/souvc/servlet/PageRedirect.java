c95e38bfc1cc51b83d74631eb10f42f333a061ae
liuhongfeng <daliu_it@163.com>
2019-05-22 18:03:51 +0800

    优化重定向代码

--- Servlet-Page-Redirect/src/main/java/com/souvc/servlet/PageRedirect.java
+++ Servlet-Page-Redirect/src/main/java/com/souvc/servlet/PageRedirect.java
@@ -1,26 +1,28 @@
 package com.souvc.servlet;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class PageRedirect extends HttpServlet{
- 
-	private static final long serialVersionUID = 1L;
+/**
+ * 页面重定向
+ *
+ * @author souvc
+ */
+public class PageRedirect extends HttpServlet {
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-		
-      // 设置响应内容类型
-      response.setContentType("text/html;charset=UTF-8");
+    private static final long serialVersionUID = 1L;
 
-      // 要重定向的新位置
-      String site = new String("http://www.souvc.com");
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
 
-      response.setStatus(response.SC_MOVED_TEMPORARILY);
-      response.setHeader("Location", site);    
+        // 设置响应内容类型
+        response.setContentType("text/html;charset=UTF-8");
+
+        // 要重定向的新位置
+        String site = "http://www.souvc.com";
+
+        response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+        response.setHeader("Location", site);
     }
 } 
--- Servlet-Page-Redirect/src/main/java/com/souvc/servlet/PageRedirect2.java
+++ Servlet-Page-Redirect/src/main/java/com/souvc/servlet/PageRedirect2.java
@@ -7,16 +7,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class PageRedirect2 extends HttpServlet{
- 
-	private static final long serialVersionUID = 1L;
+/**
+ * @author 重定向
+ */
+public class PageRedirect2 extends HttpServlet {
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-		
-      // 设置响应内容类型
-      response.setContentType("text/html;charset=UTF-8");
-      response.sendRedirect("http://www.souvc.com");
-      
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        // 设置响应内容类型
+        response.setContentType("text/html;charset=UTF-8");
+        response.sendRedirect("http://www.souvc.com");
+
     }
 } 
